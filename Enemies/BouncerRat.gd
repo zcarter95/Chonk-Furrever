0feee026488b7b30446646de1a6e24c2ebcbc9b1
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2022-09-01 21:40:13 -0700

    Implemented Bouncer Runaway Animation

--- Enemies/BouncerRat.gd
+++ Enemies/BouncerRat.gd
@@ -1,6 +1,21 @@
 extends StaticBody2D
 
+onready var animatedSprite = $AnimatedSprite
+onready var animationPlayer = $AnimationPlayer
+
 func _on_Area2D_body_entered(body):
 	if body is Player:
 		if body.cat_state == 3:
+			animatedSprite.animation = "Scared"
+
+
+func _on_AnimatedSprite_animation_finished():
+	animatedSprite.animation = "Runaway"
+	animationPlayer.play("Runaway")
+	
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	match anim_name:
+		"Runaway":
 			queue_free()
