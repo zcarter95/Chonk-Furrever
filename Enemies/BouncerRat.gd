e0972642f6d1e9195f2da122accd56a2bd73b7b7
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2022-09-02 21:02:47 -0700

    Added alternate text for bouncer

--- Enemies/BouncerRat.gd
+++ Enemies/BouncerRat.gd
@@ -5,14 +5,17 @@ onready var animationPlayer = $AnimationPlayer
 onready var timer = $Timer
 
 var new_dialog = Dialogic.start("Bouncer")
+var other_dialog = Dialogic.start("BouncerKitchen")
 
 func _on_Area2D_body_entered(body):
 	if body is Player:
 		if body.cat_state == 3:
 			animatedSprite.animation = "Scared"
 			timer.start()
-		else:
+		elif get_tree().current_scene.name == "Overworld":
 			get_parent().add_child(new_dialog)
+		elif get_tree().current_scene.name == "Kitchen":
+			get_parent().add_child(other_dialog)
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	match anim_name:
@@ -23,3 +26,6 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 func _on_Timer_timeout():
 	animatedSprite.animation = "Runaway"
 	animationPlayer.play("Runaway")
+	
+func _ready():
+	print_debug(get_tree().current_scene)
